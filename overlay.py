a1c63a65869c3d5514b6fb48b81b40d34cd4e0db
Mateus Munhoz <mateusimunhoz@hotmail.com>
2026-07-25 15:13:50 -0300

    Tem como especificar o mapa que esta, qual spawn pegou e salva eles em pastas diferentes

--- overlay.py
+++ overlay.py
@@ -1,33 +1,40 @@
 import tkinter as tk
+import tkinter.font as tkfont
 import subprocess
 import os
 import sys
 from pynput import keyboard
 from PIL import Image, ImageTk
+from tkinter import messagebox
+import time
 
 class Overlay:
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("Print Timer")
-        self.root.geometry("220x90+1400+20")
+        self.root.geometry("400x140+960+540")
         self.root.wm_attributes('-topmost', True)
         self.root.configure(bg='#2b2b2b')
 
+        fonte_padrao = tkfont.nametofont("TkDefaultFont")
+        fonte_padrao.configure(family="JetBrains Mono", size=10)
+        self.root.option_add("*Font", fonte_padrao)
+
         self.label = tk.Label(
-            self.root, text="Proximo: 15:00 | F8",
-            font=("Consolas", 14), fg="#ffffff", bg='#2b2b2b'
+            self.root, text="Lembra de tirar| F8",
+            font=("JetBrains Mono", 14), fg="#ffffff", bg='#2b2b2b'
         )
         self.label.pack(pady=6)
 
         self.btn = tk.Button(
             self.root, text="ATIVAR SCRIPT",
-            font=("Consolas", 11, "bold"),
             bg="#3a3a3a", fg="#ffffff",
             activebackground="#555555", activeforeground="#ffffff",
-            relief=tk.FLAT, bd=0,
+            relief="flat", bd=0,
             command=self.abrir_popup
         )
         self.btn.pack(pady=2)
+
 
         self.seconds = 900
         self.alerta_ativo = False
@@ -36,8 +43,6 @@ class Overlay:
         listener.daemon = True
         listener.start()
 
-        self.exibir_mapa()
-        self.contagem()
         self.root.mainloop()
 
     def exibir_mapa(self):
@@ -48,76 +53,103 @@ class Overlay:
         self.janela_mapa.geometry("+20+20")
         self.janela_mapa.configure(bg='#2b2b2b')
 
-        for nome in ["DireitaSpawns.png", "EsquerdaSpawns.png"]:
+        for nome in ["EsquerdaSpawns.png", "DireitaSpawns.png"]:
             caminho = os.path.join(script_dir, nome)
             if os.path.exists(caminho):
                 img = Image.open(caminho)
                 foto = ImageTk.PhotoImage(img)
                 lbl = tk.Label(self.janela_mapa, image=foto, bg='#2b2b2b')
                 lbl.image = foto
-                lbl.pack(side=tk.LEFT, padx=2)
+                lbl.pack(side="left", padx=2)
 
     def on_press(self, key):
         if hasattr(key, 'name') and key.name == 'f8':
             self.root.after(0, self.abrir_popup)
 
-    def contagem(self):
-        mins, secs = divmod(self.seconds, 60)
-        self.label.config(text=f"Proximo: {mins}:{secs:02d}")
 
-        if self.seconds <= 0:
-            if not self.alerta_ativo:
-                self.label.config(text="APERTE AGORA!", fg="#ff4444")
-                self.root.configure(bg='#661111')
-                self.label.configure(bg='#661111')
-                self.btn.configure(bg='#cc3333', text=">>> ATIVAR <<<")
-                self.alerta_ativo = True
-        else:
-            self.seconds -= 1
-
-        self.root.after(1000, self.contagem)
 
     def abrir_popup(self):
         popup = tk.Toplevel(self.root)
         popup.title("Spawn")
-        popup.geometry("200x90+1500+120")
+        popup.geometry("250x250+1500+120")
         popup.wm_attributes('-topmost', True)
         popup.configure(bg='#2b2b2b')
 
-        tk.Label(popup, text="Qual spawn voce pegou?",
-                 font=("Consolas", 10), fg="#ffffff", bg='#2b2b2b').pack(pady=5)
+        def so_letras(texto):
+            return texto =="" or not texto.isdigit()
 
-        entry = tk.Entry(popup, font=("Consolas", 12),
-                         bg="#3a3a3a", fg="#ffffff", insertbackground="#ffffff",
-                         relief=tk.FLAT, justify="center")
-        entry.pack(pady=2, padx=10, fill="x")
-        entry.focus_set()
+
+        tk.Label(popup, text="Qual mapa voce está ?",
+                 fg="#ffffff", bg='#2b2b2b').pack(pady=5)
+
+        teste = popup.register(so_letras)
+        entryMap = tk.Entry(popup,
+            bg="#3a3a3a", fg="#ffffff", insertbackground="#ffffff",
+            relief="flat",
+            justify="center",
+            validate = "key",
+            validatecommand=(teste, "%P"))
+        entryMap.pack(pady=2, padx=10, fill="x")
+        entryMap.focus_set()
+
+        def so_numeros(texto):
+            return texto == "" or texto.isdigit()
+
+        vcmd = popup.register(so_numeros)
+
+        tk.Label(popup, text="Qual spawn voce pegou ?",
+            fg="#ffffff", bg='#2b2b2b').pack(pady=5)
+
+        entrySpawn = tk.Entry(popup,
+            bg="#3a3a3a", fg="#ffffff", insertbackground="#ffffff",
+            relief="flat", justify="center",
+            validate="key", validatecommand=(vcmd, "%P"))
+        entrySpawn.pack(pady=2, padx=10, fill="x")
+
+        tk.Label(popup, text="Legenda de spawns",
+            fg="#ffffff", bg='#2b2b2b').pack(pady=5)
+        legenda = tk.Button(popup, text="Legenda", command=self.exibir_mapa, fg="#ffffff", bg='#2b2b2b')
+        legenda.pack(pady=2)
+
+        entryMap.bind("<Return>", lambda e: confirmar())
+
+
 
         def confirmar():
-            spawn = entry.get()
-            popup.destroy()
-            self.rodar_script(spawn)
+            mapsInfo = {"Customs": 24, "Shoreline":26}
+            spawn_str = entrySpawn.get()
+            spawn_int = int(spawn_str)
+            mapa = entryMap.get()
+
+            if mapa in mapsInfo and spawn_int <= mapsInfo[mapa]:
+                self.rodar_script(spawn_str, mapa)
+                tk.Label(popup, text="Tirando prints",fg="#ffffff", bg='#2b2b2b').pack(pady=5)
+                popup.destroy()
+            else:
+                messagebox.showerror("Erro, spawn invalido", f"Esse mapa só tem {mapsInfo[mapa]} spawns")
 
 
-        entry.bind("<Return>", lambda e: confirmar())
+        entrySpawn.bind("<Return>", lambda e: confirmar())
 
-        tk.Button(popup, text="OK", font=("Consolas", 10, "bold"),
-                  bg="#4a4a4a", fg="#ffffff",
-                  activebackground="#666666", activeforeground="#ffffff",
-                  relief=tk.FLAT, command=confirmar).pack(pady=2)
+        tk.Button(popup, text="OK",
+            bg="#4a4a4a", fg="#ffffff",
+            activebackground="#666666", activeforeground="#ffffff",
+            relief="flat", command=confirmar).pack(pady=2)
 
-    def rodar_script(self, spawn):
+    def rodar_script(self, spawn, mapa):
         script_dir = os.path.dirname(os.path.abspath(__file__))
-        subprocess.Popen(
-            [sys.executable, os.path.join(script_dir, "pytorchTraining.py"), spawn],
+        self.root.withdraw()
+        self.processo = subprocess.Popen(
+            [sys.executable, os.path.join(script_dir, "pytorchTraining.py"), spawn, mapa],
             cwd=script_dir
         )
-        self.seconds = 900
-        self.alerta_ativo = False
-        self.label.config(text="Proximo: 5:00", fg="#ffffff")
-        self.root.configure(bg='#2b2b2b')
-        self.label.configure(bg='#2b2b2b')
-        self.btn.configure(bg='#3a3a3a', text="ATIVAR SCRIPT")
+        self.root.after(500, self.verificar_processo)
+
+    def verificar_processo(self):
+        if self.processo.poll() is not None:
+            self.root.destroy()
+        else:
+            self.root.after(500, self.verificar_processo)
 
 
 if __name__ == "__main__":
--- pytorchTraining.py
+++ pytorchTraining.py
@@ -1,3 +1,5 @@
+from multiprocessing import process
+
 import mss
 import mss.tools
 import os
@@ -9,19 +11,23 @@ import time
 downloads_path = Path.home() / "Downloads"
 caminho_imagens = downloads_path / "caminho_imagens"
 
-numSpawn = sys.argv[1] if len(sys.argv) > 1 else input("Qual spawn voce pegou? ")
 
-imgSave = caminho_imagens / f"spawn{numSpawn}"
-os.makedirs(imgSave, exist_ok=True)
+numSpawn = sys.argv[1] if len(sys.argv) > 1 else input("Qual spawn voce pegou? ")
+mapName = sys.argv[2]
+
+
+imgsaveMapSel = caminho_imagens / f"{mapName}" / f"spawn{numSpawn}"
+os.makedirs(imgsaveMapSel, exist_ok=True)
 
 json_path = Path("C:/Users/mateu/Downloads/AITraining")
 with open(json_path / "spawnCounter.json", "w") as f:
-    json.dump({"numSpawn": numSpawn}, f)
+    json.dump({"numSpawn": numSpawn, "mapName": mapName}, f)
+
 
 with mss.MSS() as sct:
     contador = 0
     while contador < 15:
         contador += 1
-        sct.shot(output=str(imgSave / f"{contador}.png"))
-        time.sleep(1)
+        sct.shot(output=str(imgsaveMapSel / f"{contador}.png"))
+        time.sleep(0.5)
 
